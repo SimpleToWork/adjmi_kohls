21477cb6bb6180a6e22bea9aa3b001742346e3c9
Nathan Mosseri <nathan@simpletowork.com>
2024-10-31 17:08:30 -0400

    functions more independant

--- get_charges.py
+++ get_charges.py
@@ -1,29 +1,11 @@
-from models import (
-	Base,
-	Calendar,
-	Charge
-)
-import pandas as pd
-import time
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from models import Calendar
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 from csv_scraper import setup_driver, login, navigate_to_charges, fill_search_criteria, export_charges_csv
+from main import setup_database
 
 
-def setup_database():
-	connection_string = "mysql+mysqlconnector://root:Simple123@localhost/adjmi_kohls"
-	engine = create_engine(connection_string, echo=False)
-	Base.metadata.create_all(engine)
-	Session = sessionmaker(bind=engine)
-	session = Session()
-	return engine, session
-
-engine, session = setup_database()
-
-
-def enable_past_calendars(months_back):
+def enable_past_calendars(session, months_back):
 	try:
 		today = datetime.today()
 		last_of_current_month = (today + relativedelta(months=1)).replace(day=1) - timedelta(days=1)
@@ -37,6 +19,7 @@ def enable_past_calendars(months_back):
 
 		for c in previous_calendars:
 			c.pulled = False
+			c.imported = False
 
 		session.commit()
 		print(f"Enabled past {months_back} months: {previous_calendars}")
@@ -44,7 +27,7 @@ def enable_past_calendars(months_back):
 		print(f"An error occurred trying to enable the past {months_back} months.")
 
 
-def get_unpulled_calendars():
+def get_unpulled_calendars(session):
 	print(f"\nGetting unpulled months...")
 	calendars = session.query(Calendar).filter_by(pulled=False).all()
 	if len(calendars) > 0:
@@ -54,7 +37,7 @@ def get_unpulled_calendars():
 	return calendars
 
 
-def mark_calendar_pulled(month):
+def mark_calendar_pulled(session, month):
 	try:
 		month.pulled = True
 		session.commit()
@@ -63,11 +46,40 @@ def mark_calendar_pulled(month):
 		print(f"An error occurred while trying to marked month as pulled: {e}")
 
 
+def get_charges_process(session, driver):
+	try:
+		enable_past_calendars(session, 3)
+
+		months_to_search = get_unpulled_calendars(session)
+
+		print("\nLogging in...")
+		login(driver)
+
+		print(f"\n{len(months_to_search)} Months to search")
+		for month in months_to_search:
+			start = month.start_date.strftime('%m/%d/%Y')
+			end = month.end_date.strftime('%m/%d/%Y')
+			print("\nNavigating to Charges Search...")
+			navigate_to_charges(driver)
+			print(f"Filling search criteria for {month.month}-{month.year}...")
+			fill_search_criteria(driver, start, end)
+			print("Clicking Export to CSV button...")
+			export_charges_csv(driver, month)
+			print("CSV saved and renamed.")
+			print(f"Marking {month.month}-{month.year} as pulled...")
+			mark_calendar_pulled(session, month)
+
+			# time.sleep(4)
+	except Exception as e:
+		print(f"\nError while getting charges data: {e}")
+
+
 if __name__ == '__main__':
 	try:
-		enable_past_calendars(3)
+		engine, session = setup_database()
+		enable_past_calendars(session, 3)
 
-		months_to_search = get_unpulled_calendars()
+		months_to_search = get_unpulled_calendars(session)
 
 		print("\nStarting Web Driver...")
 		driver = setup_driver()
@@ -87,9 +99,9 @@ if __name__ == '__main__':
 			export_charges_csv(driver, month)
 			print("CSV saved and renamed.")
 			print(f"Marking {month.month}-{month.year} as pulled...")
-			mark_calendar_pulled(month)
+			mark_calendar_pulled(session, month)
 
-			time.sleep(4)
+			# time.sleep(4)
 	except Exception as e:
 		print(f"\nError while getting charges data: {e}")
 
