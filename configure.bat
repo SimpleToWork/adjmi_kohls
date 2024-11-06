939c4b637fd59339d4ee2f3bd938ff999c273f56
Nathan Mosseri <nathan@simpletowork.com>
2024-11-06 10:54:49 -0500

    adjusted paths

--- configure.bat
+++ configure.bat
@@ -2,13 +2,13 @@
 
 pip install virtualenv
 
-cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\adjmi_kohls"
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Adjmi Apparel\adjmi_kohls"
 python -m virtualenv venv
 
 cd venv/scripts
 call activate.bat
 
-cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Nathan's\adjmi_kohls"
+cd /d "C:\Users\%USERNAME%\Desktop\New Projects\Adjmi Apparel\adjmi_kohls"
 pip install -r requirements.txt
 
 
